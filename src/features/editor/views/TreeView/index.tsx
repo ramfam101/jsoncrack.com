8ce2f75a8fce7b17231cd33aa1287d97aceabadd
njneered <n.lacambra@ufl.edu>
2025-07-28 17:02:32 -0400

    Add edit, save, and cancel

--- src/features/editor/views/TreeView/index.tsx
+++ src/features/editor/views/TreeView/index.tsx
@@ -8,12 +8,36 @@ import { Value } from "./Value";
 export const TreeView = () => {
   const theme = useTheme();
   const json = useJson(state => state.json);
+  const setJson = useJson(state => state.setJson);
+
+  const handleEditValue = (keyPath: string[], newValue: unknown) => {
+    const parsed = JSON.parse(json);
+    let obj = parsed;
+    // Traverse to the parent of the target key
+    for (let i = keyPath.length - 1; i > 0; i--) {
+      obj = obj[keyPath[i]];
+    }
+    obj[keyPath[0]] = newValue;
+    setJson(JSON.stringify(parsed, null, 2));
+  };
 
   return (
     <JSONTree
       hideRoot
       data={JSON.parse(json)}
-      valueRenderer={(valueAsString, value) => <Value {...{ valueAsString, value }} />}
+      valueRenderer={(valueAsString, value, ...rest) => {
+        const keyPath = rest[1];
+        if (!Array.isArray(keyPath)) {
+          return <Value valueAsString={valueAsString} value={value} />;
+        }
+        return (
+          <Value
+            valueAsString={valueAsString}
+            value={value}
+            onEditValue={newValue => handleEditValue(keyPath as string[], newValue)}
+          />
+        );
+      }}
       labelRenderer={(keyPath, nodeType) => <Label {...{ keyPath, nodeType }} />}
       theme={{
         extend: {
